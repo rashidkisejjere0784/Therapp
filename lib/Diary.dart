f3912e54b0f8e5f419b03acaa06de8a48ea2dece
Kisejjere Rashid <88730649+rashidkisejjere0784@users.noreply.github.com>
2023-03-31 00:23:51 +0600

    Update Diary.dart

--- lib/Diary.dart
+++ lib/Diary.dart
@@ -1,23 +1,25 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:therapp/Pallete.dart';
 import 'Message.dart';
 import 'package:grouped_list/grouped_list.dart';
 import 'package:bubble/bubble.dart';
-import 'package:therapp/DiaryNoteBook.dart';
 import 'DiaryPage.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:openai_client/openai_client.dart';
+// ignore: implementation_imports
 import 'package:openai_client/src/model/openai_chat/chat_message.dart';
 
 class Diary extends StatefulWidget {
-  Diary({Key? key}) : super(key: key);
+  const Diary({Key? key}) : super(key: key);
+  @override
   _WidgetState createState() => _WidgetState();
 }
 
 class _WidgetState extends State<Diary> {
-  bool _Diary = true;
+  // ignore: non_constant_identifier_names
+  var _Diary = true;
   bool _isloading = false;
 
   var inputController = TextEditingController();
@@ -25,19 +27,19 @@ class _WidgetState extends State<Diary> {
   int Limit = 7;
 
   List<ChatMessage> SharedMessages = [
-    ChatMessage(
+    const ChatMessage(
         role: "system",
         content:
-            "You are called \'Therapp Bot\' a proffessional therapist with the aim of making the user feel better")
+            "You are called \'Therapp Bot\' a proffessional therapist and psychologist. i will provide you with thoughts and feelings. i want to you to give me scientific suggestions that will make me feel better. please make sure to listen first few times before giving any suggestions. get to know that i am very delicate and all your responses should be very proffessional")
   ];
 
   List<Message> messages = [
-    Message(text: "Hello, Welcome to Lets Chat by TherApp. ", isFromMe: false),
-    Message(
+    const Message(text: "Hello, Welcome to Lets Chat by TherApp. ", isFromMe: false),
+    const Message(
         text:
             "Here you will be able to share and interact with a chatbot that was trained on real world conversations between a Therapist and a patient. ",
         isFromMe: false),
-    Message(
+    const Message(
         text:
             "Non of the information you share here will be recorded. please note that the model is still in development and some output might not be accurate.",
         isFromMe: false)
@@ -50,8 +52,8 @@ class _WidgetState extends State<Diary> {
     SharedMessages.add(ChatMessage(role: role, content: message));
   }
 
-  final conf = OpenAIConfiguration(
-      apiKey: 'API KEY');
+  final conf = const OpenAIConfiguration(
+      apiKey: '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');
 
   Future<void> generate(String message) async {
     String response = "";
@@ -91,45 +93,40 @@ class _WidgetState extends State<Diary> {
         elevation: 0,
         title: Row(
           children: [
-            Container(
-                child: GestureDetector(
+            GestureDetector(
               onTap: (() {
-                setState(
-                  () {
-                    _Diary = true;
-                  },
-                );
+            setState(
+              () {
+                _Diary = true;
+              },
+            );
               }),
               child: Text(
-                'Your Diary',
-                style: TextStyle(
-                  color: _Diary ? Pallete.dark_purple : Pallete.text_color1,
-                ),
+            'Your Diary',
+            style: TextStyle(
+              color: _Diary ? Pallete.dark_purple : Pallete.text_color1,
+            ),
               ),
-            )),
-            Spacer(),
-            Container(
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _Diary = false;
-                  });
-                },
-                child: Container(
-                  child: Text(
-                    "Let's talk",
-                    style: TextStyle(
-                      color:
-                          !_Diary ? Pallete.dark_purple : Pallete.text_color1,
-                    ),
-                  ),
+            ),
+            const Spacer(),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _Diary = false;
+                });
+              },
+              child: Text(
+                "Let's talk",
+                style: TextStyle(
+                  color:
+                      !_Diary ? Pallete.dark_purple : Pallete.text_color1,
                 ),
               ),
             ),
           ],
         ),
       ),
-      body: _Diary ? DiaryPage() : buildChat(),
+      body: _Diary ? const DiaryPage() : buildChat(),
     );
   }
 
@@ -138,14 +135,14 @@ class _WidgetState extends State<Diary> {
       children: [
         Expanded(
           child: GroupedListView<Message, DateTime>(
-            padding: EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
             useStickyGroupSeparators: true,
             elements: messages,
             groupBy: (message) => DateTime(2022),
-            groupHeaderBuilder: (Message message) => SizedBox(),
+            groupHeaderBuilder: (Message message) => const SizedBox(),
             itemBuilder: (context, Message message) => Bubble(
               color: message.isFromMe ? Pallete.dark_purple : Colors.white,
-              margin: BubbleEdges.only(top: 10),
+              margin: const BubbleEdges.only(top: 10),
               alignment:
                   message.isFromMe ? Alignment.topRight : Alignment.topLeft,
               child: Text(
@@ -160,7 +157,7 @@ class _WidgetState extends State<Diary> {
         Row(
           children: [
             Container(
-              margin: EdgeInsets.only(left: 10),
+              margin: const EdgeInsets.only(left: 10),
               height: 40,
               width: 40,
               decoration: BoxDecoration(
@@ -169,14 +166,14 @@ class _WidgetState extends State<Diary> {
               ),
               child: GestureDetector(
                 onTap: (() {}),
-                child: Icon(
+                child: const Icon(
                   Icons.mic,
                   size: 20,
                   color: Colors.white,
                 ),
               ),
             ),
-            Container(
+            SizedBox(
               width: MediaQuery.of(context).size.width - 100,
               child: TextField(
                 cursorColor: Pallete.dark_purple,
@@ -196,7 +193,6 @@ class _WidgetState extends State<Diary> {
                 onSubmitted: (value) {
                   if (!_isloading) {
                     final mess = Message(text: value, isFromMe: true);
-                    print("subm");
                     setState(() {
                       inputController.text = "";
                       messages.add(mess);
@@ -240,44 +236,4 @@ class _WidgetState extends State<Diary> {
       ],
     );
   }
-
-  // void getMessage(String text) async {
-  //   var response = await getResponse(text);
-  //   if (response != null) {
-  //     Map<String, dynamic> output = json.decode(response.body);
-  //     String message = output["output"];
-  //     int lindex = message.lastIndexOf("-->");
-  //     message = message.substring(lindex);
-  //     message = message.replaceAll("<|endoftext|>", "");
-  //     message = message.replaceAll("&nbsp;", ".");
-  //     message = message.replaceAll("\n", " ");
-  //     Message ms = Message(text: message, isFromMe: false);
-  //     setState(() {
-  //       messages.add(ms);
-  //       _isloading = false;
-  //     });
-  //   } else {
-  //     setState(() {
-  //       messages.add(Message(
-  //           text: "Failed to get response. check your internet connection",
-  //           isFromMe: false));
-  //       _isloading = false;
-  //     });
-  //   }
-  //   print("done");
-  // }
-
-  // Future<http.Response?> getResponse(String text) async {
-  //   var url = 'https://therapp-prediction-n5yjitcc5q-lm.a.run.app/predict';
-  //   text = text + " -->";
-
-  //   Map data = {'text': text};
-  //   //encode Map to JSON
-  //   var body = json.encode(data);
-  //   print("here");
-  //   var response = await http.post(Uri.parse(url),
-  //       headers: {"Content-Type": "application/json"}, body: body);
-
-  //   return response;
-  // }
 }
