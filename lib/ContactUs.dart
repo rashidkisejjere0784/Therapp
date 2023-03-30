a50a42eacbe2c0e5a502990a5039d0b18741aa44
Kisejjere Rashid <88730649+rashidkisejjere0784@users.noreply.github.com>
2023-03-31 00:22:17 +0600

    Update ContactUs.dart

--- lib/ContactUs.dart
+++ lib/ContactUs.dart
@@ -1,9 +1,12 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'Pallete.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ContactUs extends StatefulWidget {
   const ContactUs({Key? key}) : super(key: key);
+  @override
   _ContactUsState createState() => _ContactUsState();
 }
 
@@ -19,9 +22,9 @@ class _ContactUsState extends State<ContactUs> {
         backgroundColor: Pallete.background,
         appBar: AppBar(
           backgroundColor: Colors.white,
-          iconTheme: IconThemeData(color: Pallete.dark_purple),
+          iconTheme: const IconThemeData(color: Pallete.dark_purple),
           elevation: 0,
-          title: Center(
+          title: const Center(
             child: Text(
               "Leave a Feedback",
               style: TextStyle(color: Pallete.dark_purple, fontSize: 20),
@@ -29,105 +32,107 @@ class _ContactUsState extends State<ContactUs> {
           ),
           centerTitle: true,
         ),
-        body: Column(children: [
-          SizedBox(height: 10),
-          Container(
-            color: Colors.white,
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: TextFormField(
-                controller: nameController,
-                style: TextStyle(
-                    color: Pallete.dark_purple,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22),
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    prefixIcon: Icon(Icons.person, color: Pallete.dark_purple),
-                    labelText: "NAME",
-                    labelStyle: TextStyle(
-                        color: Colors.grey[400], fontWeight: FontWeight.w800))),
-          ),
-          SizedBox(height: 10),
-          Container(
-            color: Colors.white,
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: TextFormField(
-              controller: phoneController,
-              style: TextStyle(
-                  color: Pallete.dark_purple,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22),
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                icon: const Icon(Icons.phone),
-                hintText: "Phone Number",
-                labelText: "Phone",
-                labelStyle: TextStyle(
-                    color: Colors.grey[400], fontWeight: FontWeight.w800),
-                prefixText: '+256 ',
-              ),
-              keyboardType: TextInputType.phone,
-              onSaved: (value) {},
-              maxLength: 9,
-              // TextInputFormatters are applied in sequence.
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+            const SizedBox(height: 10),
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: TextFormField(
+                  controller: nameController,
+                  style: const TextStyle(
+                      color: Pallete.dark_purple,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22),
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: const Icon(Icons.person, color: Pallete.dark_purple),
+                      labelText: "NAME",
+                      labelStyle: TextStyle(
+                          color: Colors.grey[400], fontWeight: FontWeight.w800))),
             ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            color: Colors.white,
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: TextFormField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                style: TextStyle(
+            const SizedBox(height: 10),
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: TextFormField(
+                controller: phoneController,
+                style: const TextStyle(
                     color: Pallete.dark_purple,
                     fontWeight: FontWeight.bold,
                     fontSize: 22),
                 decoration: InputDecoration(
-                    border: InputBorder.none,
-                    prefixIcon: Icon(Icons.email, color: Pallete.dark_purple),
-                    labelText: "EMAIL",
-                    labelStyle: TextStyle(
-                        color: Colors.grey[400], fontWeight: FontWeight.w800))),
-          ),
-          SizedBox(height: 10),
-          Container(
-            color: Colors.white,
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: TextFormField(
-              controller: bodyController,
-              decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  hintText: "Enter Your Message",
-                  helperText: "Your FeedBack is highly appreciated",
-                  labelText: "Message",
+                  border: InputBorder.none,
+                  icon: const Icon(Icons.phone),
+                  hintText: "Phone Number",
+                  labelText: "Phone",
                   labelStyle: TextStyle(
-                      color: Colors.grey[400], fontWeight: FontWeight.w800)),
-              maxLines: 7,
-            ),
-          ),
-          SizedBox(height: 10),
-          Center(
-            child: ElevatedButton(
-              onPressed: () async {
-                await _launchURL();
-              },
-              child: Text("Submit"),
-              style: ElevatedButton.styleFrom(
-                primary: Pallete.dark_purple,
-                textStyle: const TextStyle(
-                    color: Pallete.dark_purple,
-                    fontSize: 30,
-                    fontStyle: FontStyle.normal),
+                      color: Colors.grey[400], fontWeight: FontWeight.w800),
+                  prefixText: '+256 ',
+                ),
+                keyboardType: TextInputType.phone,
+                onSaved: (value) {},
+                maxLength: 9,
+                // TextInputFormatters are applied in sequence.
               ),
             ),
-          ),
-        ]));
+            const SizedBox(height: 10),
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  style: const TextStyle(
+                      color: Pallete.dark_purple,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22),
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: const Icon(Icons.email, color: Pallete.dark_purple),
+                      labelText: "EMAIL",
+                      labelStyle: TextStyle(
+                          color: Colors.grey[400], fontWeight: FontWeight.w800))),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: TextFormField(
+                controller: bodyController,
+                decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: "Enter Your Message",
+                    helperText: "Your FeedBack is highly appreciated",
+                    labelText: "Message",
+                    labelStyle: TextStyle(
+                        color: Colors.grey[400], fontWeight: FontWeight.w800)),
+                maxLines: 7,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  await _launchURL();
+                },
+                child: const Text("Submit"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Pallete.dark_purple,
+                  textStyle: const TextStyle(
+                      color: Pallete.dark_purple,
+                      fontSize: 30,
+                      fontStyle: FontStyle.normal),
+                ),
+              ),
+            ),
+          ]),
+        ));
   }
 
   _launchURL() async {
     String name = nameController.text.trim();
-    String phone = phoneController.text.trim();
     String email = emailController.text.trim();
     String body = bodyController.text.trim();
     String toMailId = "rashidkisejjere0784@gmail.com";
@@ -136,7 +141,6 @@ class _ContactUsState extends State<ContactUs> {
     var url = 'mailto:$toMailId?subject=$subject&body=$body&from=$email';
     if (await canLaunch(url)) {
       await launch(url);
-      print("success");
     } else {
       throw 'Could not launch $url';
     }
